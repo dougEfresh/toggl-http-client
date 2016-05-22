b0c8930e45173595fb9a7eb6bfe22d5b24c76f63
Douglas Chimento <dchimento@gmail.com>
2016-05-22 13:22:59 -0400

    adding files

---
+++ doc.go
@@ -0,0 +1,30 @@
+// Gtoggl - A go client library for toggl
+package gtoggl
+/*
+import "github/dougEfresh/gtoggl"
+
+func main() {
+	client, err := NewClient("")
+	if err == nil {
+		t.Fatal("Error should have been thrown. No Token given")
+	}
+	client, err = NewClient("test-token")
+
+	if client == nil {
+		t.Fatal(err)
+	}
+
+	if len(client.token) < 1 {
+		t.Errorf("Token not defined %d", len(client.token))
+	}
+}
+
+
+package main
+
+import "testing"
+
+func TestClientDefaults(t *testing.T) {
+
+}
+*/
