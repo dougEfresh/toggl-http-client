80543732c72b00d670a3ee91775dab32c28f3ae8
Douglas Chimento <dchimento@gmail.com>
2016-05-23 13:01:21 -0400

    adding gproject

---
+++ gproject/project.go
@@ -0,0 +1,170 @@
+package gproject
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dougEfresh/gtoggl"
+)
+
+// Toggl Client Definition
+type Client struct {
+	Id       uint64 `json:"id"`
+	WId      uint64 `json:"wid"`
+	Name     string `json:"name"`
+	Currency string `json:"currency"`
+}
+
+type Clients []Client
+
+//Return a Toggl Client. An error is also returned when some configuration option is invalid
+//    thc,err := gtoggl.NewClient("token")
+//    tc,err := gclient.NewClient(tc)
+func NewClient(thc *gtoggl.TogglHttpClient, options ...ToggleClientOptionFunc) (*TogglClient, error) {
+	tc := &TogglClient{
+		thc:             thc,
+		listTransport:   defaultClientTransport,
+		getTransport:    defaultClientTransport,
+		updateTransport: defaultClientTransport,
+		createTransport: defaultClientTransport,
+		deleteTransport: defaultClientTransport,
+	}
+	// Run the options on it
+
+	for _, option := range options {
+		if err := option(tc); err != nil {
+			return nil, err
+		}
+	}
+	tc.clientEndpoint = thc.Url + "/clients"
+	return tc, nil
+}
+
+type TogglClient struct {
+	thc             *gtoggl.TogglHttpClient
+	clientEndpoint  string
+	listTransport   ClientLister
+	getTransport    ClientGetter
+	updateTransport ClientUpdater
+	createTransport ClientCreater
+	deleteTransport ClientDeleter
+}
+
+func (tc *TogglClient) List() (Clients, error) {
+	return tc.listTransport.List(tc)
+}
+
+func (tc *TogglClient) Get(id uint64) (Client, error) {
+	return tc.getTransport.Get(tc, id)
+}
+
+func (tc *TogglClient) Create(c *Client) (Client, error) {
+	return tc.createTransport.Create(tc, c)
+}
+
+func (tc *TogglClient) Update(c *Client) (Client, error) {
+	return tc.updateTransport.Update(tc, c)
+}
+
+func (tc *TogglClient) Delete(id uint64) error {
+	return tc.deleteTransport.Delete(tc, id)
+}
+
+type ClientLister interface {
+	List(tc *TogglClient) (Clients, error)
+}
+type ClientGetter interface {
+	Get(tc *TogglClient, id uint64) (Client, error)
+}
+type ClientUpdater interface {
+	Update(tc *TogglClient, c *Client) (Client, error)
+}
+type ClientCreater interface {
+	Create(tc *TogglClient, c *Client) (Client, error)
+}
+type ClientDeleter interface {
+	Delete(tc *TogglClient, id uint64) error
+}
+
+//Configures a Client.
+/*
+    func SetURL(url string) ToggleClientOptionFunc {
+	return func(c *TogglClient) error {
+	    c.Url = url
+	}
+    }
+ */
+type ToggleClientOptionFunc func(*TogglClient) error
+type clientTransport struct{}
+
+var defaultClientTransport = &clientTransport{}
+
+type clientResponse struct {
+	Data Client `json:"data"`
+}
+type clientRequest struct {
+	Data Client `json:"data"`
+}
+type clientCreateRequest struct {
+	Client Client `json:"client"`
+}
+
+//GET https://www.toggl.com/api/v8/clients/1239455
+func (cl *clientTransport) Get(tc *TogglClient, id uint64) (Client, error) {
+	body, err := tc.thc.GetRequest(fmt.Sprintf("%s/%d", tc.clientEndpoint, id))
+	if err != nil {
+		return Client{}, err
+	}
+
+	var aux clientResponse
+	err = json.Unmarshal(body, &aux)
+	return aux.Data, err
+}
+
+//DELETE https://www.toggl.com/api/v8/clients/1239455
+func (cl *clientTransport) Delete(tc *TogglClient, id uint64) error {
+	_, err := tc.thc.DeleteRequest(fmt.Sprintf("%s/%d", tc.clientEndpoint, id), nil)
+	return err
+}
+
+//GET https://www.toggl.com/api/v8/clients/1239455
+func (cl *clientTransport) List(tc *TogglClient) (Clients, error) {
+	body, err := tc.thc.GetRequest(tc.clientEndpoint)
+	var Clients []Client
+	if err != nil {
+		return Clients, err
+	}
+	err = json.Unmarshal(body, &Clients)
+	return Clients, err
+}
+
+//PUT https://www.toggl.com/api/v8/clients/1239455
+func (cl *clientTransport) Update(tc *TogglClient, c *Client) (Client, error) {
+	put := clientCreateRequest{Client: *c}
+	body, err := json.Marshal(put)
+	if err != nil {
+		return Client{}, err
+	}
+	response, err := tc.thc.PutRequest(fmt.Sprintf("%s/%d", tc.clientEndpoint, c.Id), body)
+	if err != nil {
+		return Client{}, err
+	}
+	var aux clientResponse
+	err = json.Unmarshal(response, &aux)
+	return aux.Data, err
+}
+
+//POST https://www.toggl.com/api/v8/clients
+func (cl *clientTransport) Create(tc *TogglClient, c *Client) (Client, error) {
+	put := clientCreateRequest{Client: *c}
+	body, err := json.Marshal(put)
+	if err != nil {
+		return Client{}, err
+	}
+	response, err := tc.thc.PostRequest(tc.clientEndpoint, body)
+	if err != nil {
+		return Client{}, err
+	}
+	var aux clientResponse
+	err = json.Unmarshal(response, &aux)
+	return aux.Data, err
+}
---
+++ gproject/project_test.go
@@ -0,0 +1,105 @@
+package gproject
+
+import (
+	"testing"
+)
+
+func togglClient() *TogglClient {
+	client := mockClient()
+	ws, err := NewClient(client)
+	if err != nil {
+		panic(err)
+	}
+	return ws
+}
+
+func TestClientCreate(t *testing.T) {
+	tClient := togglClient()
+	c := &Client{Name: "Very Big Company", WId: 777}
+	nc, err := tClient.Create(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if nc.Name != "Very Big Company" {
+		t.Fatal("!= Very Big Company")
+	}
+
+	if nc.Id != 1239455 {
+		t.Fatal("!= 1239455")
+	}
+
+	if nc.WId != 777 {
+		t.Fatal("!= 777")
+	}
+}
+
+func TestClientUpdate(t *testing.T) {
+	tClient := togglClient()
+	c := &Client{Id: 1, Name: "new name", WId: 777}
+	nc, err := tClient.Update(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if nc.Name != "new name" {
+		t.Fatal("!= new name")
+	}
+}
+
+func TestClientDelete(t *testing.T) {
+	tClient := togglClient()
+	c := &Client{Id: 1, Name: "new name", WId: 777}
+	err := tClient.Delete(c.Id)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClientList(t *testing.T) {
+	tClient := togglClient()
+	clients, err := tClient.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(clients) != 2 {
+		t.Fatal("Workspace is not 2")
+	}
+	if clients[0].Id != 1 {
+		t.Error("Workspace Id is not 1")
+	}
+	if clients[0].Name != "Id 1" {
+		t.Error("Workspace name not Id ")
+	}
+
+	if clients[1].Id != 2 {
+		t.Error("Workspace Id is not 2")
+	}
+	if clients[1].Name != "Id 2" {
+		t.Error("Workspace name")
+	}
+}
+
+func TestClientGet(t *testing.T) {
+	tClient := togglClient()
+
+	client, err := tClient.Get(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if client.Id != 1 {
+		t.Error("!= 1")
+	}
+
+	if client.Name != "Id 1" {
+		t.Error("!= Id 1:  " + client.Name)
+	}
+}
+
+func BenchmarkClientTransport_Get(b *testing.B) {
+	b.ReportAllocs()
+	tClient := togglClient()
+	 for i := 0; i < b.N; i++ {
+		 tClient.Get(1)
+	 }
+}
