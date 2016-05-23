8e7ebf8ed7ef36373e681f54289346120afa6992
Douglas Chimento <dchimento@gmail.com>
2016-05-23 15:19:26 -0400

    removing old shell files

--- fmt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go fmt
-
-for i in * ; do
-[ -f $i/doc.go ] && (cd $i  && go fmt)
-done
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go install 
-
-for i in * ; do
-[ -f $i/doc.go ] && (cd $i && go install)
-done
