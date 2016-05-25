50bae9dcfb49c74ea5eca2a769edb4ab1167ea6a
Douglas Chimento <dchimento@gmail.com>
2016-05-25 15:44:09 -0400

    user update

--- guser/user.go
+++ guser/user.go
@@ -35,11 +35,7 @@ const MeWithRelatedData = "/me?with_related_data=true"
 //    uc,err := guser.NewClient(thc)
 func NewClient(thc *gtoggl.TogglHttpClient, options ...ClientOptionFunc) (*UserClient, error) {
 	tc := &UserClient{
-		thc:             thc,
-		getTransport:    defaultTransport,
-		updateTransport: defaultTransport,
-		createTransport: defaultTransport,
-		resetTransport:  defaultTransport,
+		thc: thc,
 	}
 	// Run the options on it
 	for _, option := range options {
@@ -60,39 +56,67 @@ type UserClient struct {
 	resetEndpoint   string
 	signupEndpoint  string
 	relatedEndpoint string
-	getTransport    UserGetter
-	updateTransport UserUpdater
-	createTransport UserCreator
-	resetTransport  UserResetter
 }
 
-func (c *UserClient) Get(realatedData bool) (User, error) {
-	return c.getTransport.Get(c, realatedData)
+func (c *UserClient) Get(realatedData bool) (*User, error) {
+	body, err := c.thc.GetRequest(c.endpoint)
+	if err != nil {
+		return nil, err
+	}
+	return userResponse(body)
 }
 
-func (c *UserClient) Create(email, password, timezone string) (User, error) {
-	return c.createTransport.Create(c, email, password, timezone)
+func (c *UserClient) Create(email, password, timezone string) (*User, error) {
+	up := &UserCreate{Password: password, Email: email, Timezone: timezone, CreatedWith: "gtoggl"}
+	put := createRequest{User: up}
+	body, err := json.Marshal(put)
+	if err != nil {
+		return nil, err
+	}
+	response, err := c.thc.PostRequest(c.signupEndpoint, body)
+	if err != nil {
+		return nil, err
+	}
+	return userResponse(response)
 }
 
-func (c *UserClient) Update(u *User) (User, error) {
-	return c.updateTransport.Update(c, u)
+func (c *UserClient) Update(u *User) (*User, error) {
+	up := &UserUpdate{FullName: u.FullName, Email: u.Email}
+	put := updateRequest{User: up}
+	body, err := json.Marshal(put)
+	if err != nil {
+		return nil, err
+	}
+	response, err := c.thc.PutRequest(c.endpoint, body)
+	if err != nil {
+		return nil, err
+	}
+	return userResponse(response)
 }
 
 func (c *UserClient) ResetToken() (string, error) {
-	return c.resetTransport.ResetToken(c)
+	response, err := c.thc.PostRequest(c.resetEndpoint, nil)
+	if err != nil {
+		return "", err
+	}
+	var aux string
+	err = json.Unmarshal(*response, &aux)
+	return aux, nil
+
 }
 
-type UserGetter interface {
-	Get(tc *UserClient, relatedData bool) (User, error)
-}
-type UserUpdater interface {
-	Update(tc *UserClient, c *User) (User, error)
-}
-type UserCreator interface {
-	Create(tc *UserClient, email, password, timezone string) (User, error)
-}
-type UserResetter interface {
-	ResetToken(tc *UserClient) (string, error)
+func userResponse(response *json.RawMessage) (*User, error) {
+	var tResp gtoggl.TogglResponse
+	err := json.Unmarshal(*response, &tResp)
+	if err != nil {
+		return nil, err
+	}
+	var u User
+	err = json.Unmarshal(*tResp.Data, &u)
+	if err != nil {
+		return nil, err
+	}
+	return &u, err
 }
 
 //Configures a Client.
@@ -104,13 +128,7 @@ type UserResetter interface {
     }
 */
 type ClientOptionFunc func(*UserClient) error
-type transport struct{}
 
-var defaultTransport = &transport{}
-
-type tResponse struct {
-	Data User `json:"data"`
-}
 type request struct {
 	Data User `json:"data"`
 }
@@ -119,61 +137,4 @@ type updateRequest struct {
 }
 type createRequest struct {
 	User *UserCreate `json:"user"`
-}
-
-//GET https://www.toggl.com/api/v8/me
-func (ct *transport) Get(tc *UserClient, relatedData bool) (User, error) {
-	body, err := tc.thc.GetRequest(tc.endpoint)
-	if err != nil {
-		return User{}, err
-	}
-
-	var aux tResponse
-	err = json.Unmarshal(body, &aux)
-	return aux.Data, err
-}
-
-//PUT https://www.toggl.com/api/v8/me/1239455
-func (ct *transport) Update(tc *UserClient, c *User) (User, error) {
-	up := &UserUpdate{FullName: c.FullName, Email: c.Email}
-	put := updateRequest{User: up}
-	body, err := json.Marshal(put)
-	if err != nil {
-		return User{}, err
-	}
-	response, err := tc.thc.PutRequest(tc.endpoint, body)
-	if err != nil {
-		return User{}, err
-	}
-	var aux tResponse
-	err = json.Unmarshal(response, &aux)
-	return aux.Data, err
-}
-
-//POST https://www.toggl.com/api/v8/signups
-func (ct *transport) Create(tc *UserClient, email, password, timezone string) (User, error) {
-	up := &UserCreate{Password: password, Email: email, Timezone: timezone, CreatedWith: "gtoggl"}
-	put := createRequest{User: up}
-	body, err := json.Marshal(put)
-	if err != nil {
-		return User{}, err
-	}
-	response, err := tc.thc.PostRequest(tc.signupEndpoint, body)
-	if err != nil {
-		return User{}, err
-	}
-	var aux tResponse
-	err = json.Unmarshal(response, &aux)
-	return aux.Data, err
-}
-
-//POST https://www.toggl.com/api/v8/reset_token
-func (ct *transport) ResetToken(tc *UserClient) (string, error) {
-	response, err := tc.thc.PostRequest(tc.resetEndpoint, nil)
-	if err != nil {
-		return "", err
-	}
-	var aux string
-	err = json.Unmarshal(response, &aux)
-	return aux, nil
 }
